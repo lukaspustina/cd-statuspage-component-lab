292eda99a7911e8145a535cdd45605eaa4714c5a
Jan Hoffmann <Jan.H1895@gmx.de>
2018-06-29 14:57:37 +0200

    Diverse Vereinfachungen
    *JavaScript vereinfacht
    *CSS vereinfacht
    *JavaScript und CSS in HTML eingebunden

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
-// notice the JS Library is imported already (click the gear next to JS)
-// this script also assumes jQuery is already loaded on the page.
-
-var sp = new StatusPage.page({ page: '39qnmk3k9q7f'});
+var sp = new StatusPage.page({ page: '1f3546fgc1vd'});
 
 sp.summary({
   success: function(data) {
@@ -10,25 +7,4 @@ sp.summary({
     // appends the status indicator as a class name so we can use the right color for the status light thing
     $('.color-dot').addClass(data.status.indicator);
   }
-});
-
-
-
-
-
-
-
-
-
-
-// Ignore this...it just centers the "page content here" text"
-$(function() {
-  var setContentHeight = function() {
-    var height = $(window).outerHeight(true) - $('#custom-footer').outerHeight(true) - $('#custom-header').outerHeight(true);
-  
-    $('.page-content').css('line-height',(height + 'px'));
-  }
-  setContentHeight();
-  $(window).on('resize', function() {
-    setContentHeight();});
 });
